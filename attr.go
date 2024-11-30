d003f851e04e39b01fc777a54be4e19a71099d60
Dviih <dviih2s@gmail.com>
2024-11-30 18:44:02 -0300

    init of `attr.go`.

--- attr.go
+++ attr.go
@@ -1,3 +1,31 @@
+/*
+ *     A colored handler for slog.
+ *     Copyright (C) 2024  Dviih
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package logger
+
+import (
+	"log/slog"
+	"reflect"
+	"strconv"
+	"time"
+)
+
 func (logger *Logger) attr1(s string, attribute slog.Attr) error {
 	if err := logger.write(" \u001B[0;32m"); err != nil {
 		return err
