a8ab5fc8bda9e3b122b2011eee8e4044f27f3089
Dviih <dviih2s@gmail.com>
2024-11-30 18:42:26 -0300

    Attr1 is used to not repeat code related to attribute key/name.

---
+++ attr.go
@@ -0,0 +1,28 @@
+func (logger *Logger) attr1(s string, attribute slog.Attr) error {
+	if err := logger.write(" \u001B[0;32m"); err != nil {
+		return err
+	}
+
+	if logger.group != nil {
+		if err := logger.write(append(logger.group, '.')); err != nil {
+			return err
+		}
+	}
+
+	if s != "" {
+		if err := logger.write(s + "."); err != nil {
+			return err
+		}
+	}
+
+	if err := logger.write(attribute.Key + "\u001B[0m"); err != nil {
+		return err
+	}
+
+	if err := logger.write("->"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
