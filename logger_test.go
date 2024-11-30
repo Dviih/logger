0ab04f9b7c447e9cd3712c613ca3f2fb3981b256
Dviih <dviih2s@gmail.com>
2024-11-30 18:50:33 -0300

    Test for maps.

--- logger_test.go
+++ logger_test.go
@@ -68,3 +68,11 @@ func TestLoggerFunc(t *testing.T) {
 	}))
 }
 
+func TestLoggerMap(t *testing.T) {
+	test.Info("a map of key string and value int", slog.Any("map", map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+	}))
+}
+
